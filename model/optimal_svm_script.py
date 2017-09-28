9fefffe778cb69f687ed16cf752be25a5fa9216e
Juanma Coria <juanmc2005@hotmail.com>
2017-09-28 01:07:52 -0300

    Added vectors database and code to plot data

--- model/optimal_svm_script.py
+++ model/optimal_svm_script.py
@@ -1,8 +1,26 @@
-from preprocessor import Preprocessor
-from subjclassifier import Pipeline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
+'''
+pipeline = Pipeline(Preprocessor('../raw_db.txt')).preprocess()
 
-print(Pipeline(Preprocessor('../raw_db.txt')).preprocess().optimal_svm())
+with open('../vectorized_db.csv', 'a', encoding='utf8') as f:
+    f.write('subjective,max swf-isf,avg frs,avg fro,fr frs/fro,frm,pabs,pats\n')
+    for i, v in tqdm(enumerate(pipeline.vectors), desc='Writing vectors to disk'):
+        f.write(str(pipeline.labels[i]) + ',')
+        for j, x in enumerate(v):
+            f.write(str(x))
+            if j != len(v) - 1:
+                f.write(',')
+        f.write('\n')
+'''
+data = pd.read_csv('../vectorized_db.csv', delimiter=',')
+
+sns.set(style='white', color_codes=True)
+sns.stripplot(x='Subjetiva', y='PATS', data=data, jitter=True, edgecolor='none', alpha=.40)
+sns.despine()
+plt.show()
 
 '''
 with open('optimal_partition.txt', 'a', encoding='utf8') as f:
