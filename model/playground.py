4066048d6805c99b1817a57b10e7f81747766b31
Juanma Coria <juanmc2005@hotmail.com>
2017-09-29 00:23:05 -0300

    Exported SVM optimal partition to CSV files for training and testing data

--- model/playground.py
+++ model/playground.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import seaborn as sns
+from subjclassifier import Pipeline
+from preprocessor import Preprocessor
+from classifiers import SVMClassifier
+from sklearn.model_selection import train_test_split
+from svm.data import load_data_into_classifier
 
 '''
 pipeline = Pipeline(Preprocessor('../raw_db.txt')).preprocess()
@@ -15,11 +21,21 @@ with open('../vectorized_db.csv', 'a', encoding='utf8') as f:
                 f.write(',')
         f.write('\n')
 '''
-data = pd.read_csv('../vectorized_db.csv', delimiter=',')
 
-sns.set(style='white', color_codes=True)
-sns.stripplot(x='Subjetiva', y='PATS', data=data, jitter=True, edgecolor='none', alpha=.40)
-sns.despine()
+
+# pipeline = Pipeline(Preprocessor('../raw_db.txt')).preprocess()
+# x_train, x_test, y_train, y_test = train_test_split(pipeline.vectors, pipeline.labels, test_size=0.2, stratify=pipeline.labels)
+clf = load_data_into_classifier()
+clf.fit()
+print(clf.classifier.coef_.ravel())
+# ORDEN: ('MAX SWF-ISF', 'AVG FRS', 'AVG FRO', 'FR FRS/FRO', 'FRM', 'PABS', 'PATS'))
+
+# data = pd.read_csv('../vectorized_db.csv', delimiter=',')
+
+# sns.set(style='white', color_codes=True)
+# sns.stripplot(x='Subjetiva', y='PATS', data=data, jitter=True, edgecolor='none', alpha=.40)
+# sns.pairplot(data, hue='Subjetiva', vars=('MAX SWF-ISF', 'FRM'))
+# sns.despine()
 plt.show()
 
 '''
